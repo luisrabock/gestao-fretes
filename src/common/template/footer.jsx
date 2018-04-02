d19d54b2c082d30bf6fe00f89048b1c4005b5680
Luís <luis.rabock@hotmail.com>
2018-04-02 16:15:14 -0300

    Add files via upload

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <footer className='main-footer'>
+  <strong>
+      Copyright &copy; 2018
+      <a href='#' target='_blank'> Luis R.</a>
+  </strong>
+</footer>
+);
