d565758b151d64c08436248b808321df04c7d6a5
Luis Rabock <luis.rabock@hotmail.com>
2018-04-02 22:09:32 -0300

    Exclude folders

--- gestao-de-fretes/backend/src/api/gestaoFretesService.js
+++
@@ -1,8 +0,0 @@
-const GestaoFretes = require('./gestaoFretes');
-const ErrorHandler = require('./common/erroHandler');
-
-GestaoFretes.methods(['get', 'post', 'put', 'delete']);
-GestaoFretes.updateOptions({ new: true, runValidators: true });
-GestaoFretes.after('post', ErrorHandler).after('put', ErrorHandler);
-
-module.exports = GestaoFretes;
--- gestao-de-fretes/backend/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose.connect('mongodb://localhost/gestaoFretes', {
-  useMongoClient: true,
-});
-
-// Colocar mensagens de tratamento de erros
