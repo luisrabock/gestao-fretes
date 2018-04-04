cd6be13300870eb10e618071180bc702a0010cbc
Luís <luis.rabock@hotmail.com>
2018-04-04 17:58:46 -0300

    Add files via upload

--- backend/src/api/gestaoFretesService.js
+++ backend/src/api/gestaoFretesService.js
@@ -1,8 +1,22 @@
 const GestaoFretes = require('./gestaoFretes');
 const ErrorHandler = require('./common/erroHandler');
+const moment = require('moment');
 
 GestaoFretes.methods(['get', 'post', 'put', 'delete']);
 GestaoFretes.updateOptions({ new: true, runValidators: true });
-GestaoFretes.after('post', ErrorHandler).after('put', ErrorHandler);
+//GestaoFretes.after('post', ErrorHandler).after('put', ErrorHandler);
+
+//busca os objetos daquele mês para renderizar a cidade no mapa
+const startOfMonth = moment().startOf('month').format('YYYY-MM-DD hh:mm');
+GestaoFretes.route('markers', (req, res, next) => {
+    GestaoFretes.find({emissao: {$gt:startOfMonth}})
+    .exec(function(err, results){
+        if(err) {
+           return res.send('Error')
+        } else {
+            res.json(results)
+        };
+    });
+});
 
 module.exports = GestaoFretes;
