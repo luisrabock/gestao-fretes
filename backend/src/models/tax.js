ab3dba3c443c6db94cf9737164d4189e7f63e4a7
Luís <luis.rabock@hotmail.com>
2018-04-18 17:45:09 -0300

    Add files via upload

---
+++ backend/src/models/tax.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const taxSchema = mongoose.Schema({
+    transp: {type: Number},
+    orig: String,
+    destino: Number,
+    previsao: Number,
+    pedagio: Number,
+    indice: Number,
+    seguro: Number,
+    trt: {type: Number, default: 0},
+    tas: {type: Number, default: 0},
+    tfd: {type: Number, default: 0},
+    sf: {type: Number, default: 0},
+    trf: {type: Number, default: 0},
+    ta: {type: Number, default: 0},
+    vd: {type: Number, default: 0},
+    tda: {type: Number, default: 0},
+    min: {type: Number, default: 0},
+    perc: {type: Number, default: 0},
+    pesoUn: {type: Number, default: 0},
+    pesoMin: {type: Number, default: 0},
+});
+
+module.exports = mongoose.model('tax', taxSchema);
